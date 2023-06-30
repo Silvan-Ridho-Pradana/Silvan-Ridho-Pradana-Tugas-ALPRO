45d5da77138e645d45decc4506fb626ab2b3cf58
Silvan-Ridho-Pradana <118283944+Silvan-Ridho-Pradana@users.noreply.github.com>
2023-06-30 22:11:38 +0700

    Add files via upload

---
+++ Pertemuan 13/L 2/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Sepeda {
+    string Merk = "Polygon";
+    string type = "Sepeda Gunung";
+    int Tahun = 2013;
+    string Harga = "2.000.000";
+};
+
+int main()
+{
+    Sepeda Sepeda1;
+    cout << Sepeda1.Merk << endl;
+    cout << Sepeda1.type << endl;
+    cout << Sepeda1.Tahun << endl;
+    cout << Sepeda1.Harga << endl;
+
+    return 0;
+}
